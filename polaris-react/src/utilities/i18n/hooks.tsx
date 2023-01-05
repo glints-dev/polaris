6f764c00c73097bfd5bf1f1715ecd6824b79a877
Nina Ricci Marie Benite <ninariccimarie@gmail.com>
2023-01-05 11:22:36 +0800

    remove error from i18n if not wrapped AppProvider

--- polaris-react/src/utilities/i18n/hooks.tsx
+++ polaris-react/src/utilities/i18n/hooks.tsx
@@ -7,9 +7,5 @@ import {I18nContext} from './context';
 export function useI18n() {
   const i18n = useContext(I18nContext);
 
-  if (!i18n) {
-    throw new MissingAppProviderError('No i18n was provided.');
-  }
-
   return i18n;
 }
