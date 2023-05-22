583564203352c0f3bfd7c03993fb0d6a22d297ef
Nina Benite <nina.benite@glints.com>
2023-05-22 17:49:58 +0800

    IndexTable: TableCell sticky

--- polaris-react/src/components/IndexTable/components/Cell/Cell.tsx
+++ polaris-react/src/components/IndexTable/components/Cell/Cell.tsx
@@ -7,17 +7,20 @@ export interface CellProps {
   children?: ReactNode;
   className?: string;
   flush?: boolean;
+  sticky?: boolean;
 }
 
 export const Cell = memo(function Cell({
   children,
   className,
   flush,
+  sticky,
 }: CellProps) {
   const cellClassName = classNames(
     className,
     styles.TableCell,
     flush && styles['TableCell-flush'],
+    sticky && styles['TableCell-sticky'],
   );
 
   return <td className={cellClassName}>{children}</td>;
